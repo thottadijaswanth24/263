c912e40970fe02194665773136a82c4803d6f904
thottadijaswanth24 <thottadijaswanth.24.cse@anits.edu.in>
2026-07-14 11:22:19 +0530

    Create division_binary_search.java
    week 2

---
+++ division_binary_search.java
@@ -0,0 +1,27 @@
+import.java.util.*;
+class Main {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int a=sc.nextInt();
+        int b=sc.nextInt();
+        double low=0;
+        double high=a;
+        while(low<=high)
+        {
+            double mid=(low+high)/2;
+            if(b*mid==a)
+            {
+                System.out.print(mid);
+                return;
+            }
+            else if(b*mid>a)
+            {
+                high=mid;
+            }
+            else
+            {
+                low=mid;
+            }
+        }
+    }
+}
