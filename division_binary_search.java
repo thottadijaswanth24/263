fefda7dfc23931a1d8e01953dd0081075233682b
thottadijaswanth24 <thottadijaswanth.24.cse@anits.edu.in>
2026-07-14 11:45:59 +0530

    week 3

--- division_binary_search.java
+++ division_binary_search.java
@@ -1,27 +1,38 @@
-import.java.util.*;
-class Main { 
-    public static void main(String[] args) {
-        Scanner sc=new Scanner(System.in);
-        int a=sc.nextInt();
-        int b=sc.nextInt();
-        double low=0;
-        double high=a;
-        while(low<=high)
-        {
-            double mid=(low+high)/2;
-            if(b*mid==a)
-            {
-                System.out.print(mid);
-                return;
-            }
-            else if(b*mid>a)
-            {
-                high=mid;
-            }
-            else
-            {
-                low=mid;
-            }
-        }
-    }
+class Main {
+    public static void main(String[] args) {
+        System.out.println("Start small. Ship something.");
+        double a=22,b=-7;
+        double x=a,y=b;
+        if(b<0)
+        {
+            b=b*(-1);
+        }
+        else if(a<0)
+        {
+            a=a*(-1);
+        }
+        double low=0;
+        double high=a;
+        while(low<=high)
+        {
+            double mid=(low+high)/2;
+            if(b*mid==a)
+            {
+                if(x<0||y<0)
+                {
+                    mid=mid*-1;
+                }
+                System.out.printf("%.5f",mid);
+                return;
+            }
+            if(b*mid>a)
+            {
+                high=mid;
+            }
+            else
+            {
+                low=mid;
+            }
+        }
+    }
 }
